ed881106c8099b5993cd54e779f8706bf6a9a603
deephealingvibes <alessiovisvisentin@gmail.com>
2026-09-16 23:04:25 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE='my-frequencies-v7';
+const STATIC=['./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{
+      const copy=r.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return r;
+    }).catch(()=>caches.match('./index.html')));
+  } else {
+    e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request)));
+  }
+});
