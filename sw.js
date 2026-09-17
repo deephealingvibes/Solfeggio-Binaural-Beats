b479c39bf8eed6ce4774d016e119b9fd42d4d822
deephealingvibes <alessiovisvisentin@gmail.com>
2026-09-17 11:08:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='deep-healing-vibes-v14';
+const CACHE='deep-healing-vibes-v15';
 const STATIC=['./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
