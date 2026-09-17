0bc7b9791b1bd8bba1637d40f8fe669f3d04e12b
deephealingvibes <alessiovisvisentin@gmail.com>
2026-09-17 10:03:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='my-frequencies-v8';
+const CACHE='my-frequencies-v9';
 const STATIC=['./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
